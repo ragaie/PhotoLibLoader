32b03f066a2046a52e2688535cf9537669abc4f1
Ragaie alfy Fahmey <eng.ragaie@gmail.com>
2020-01-17 14:47:14 +0200

    Initial Commit

---
+++ DownloadData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DownloadData
+//
+//  Created by Ragaie Alfy on 1/17/20.
+//  Copyright © 2020 Ragaie Alfy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
